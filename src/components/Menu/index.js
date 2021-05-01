331107b66a37d3bd2b7d4824fe978826e77f5bb8
Clebson Farias <clebson.farias@ctec.ufal.br>
2021-04-30 21:40:50 -0300

    Header

---
+++ src/components/Menu/index.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const Header = styled.header`
+  background: blue;
+  height: 1rem;
+  wight: 100%
+  margin-bottom: 0
+
+  display: flex;
+  align-items: center;
+
+  padding: 2rem;
+
+  border-bottom: 1px solid black;
+`;
+
+
+export default function Menu() {
+    return (
+        <Header>
+
+        </Header>
+    )
+}
